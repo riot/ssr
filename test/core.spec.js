b73480d3c42c5e3125b178821d0bc6c94afca802
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-03-09 23:45:42 +0100

    updated: add test for divs

--- test/core.spec.js
+++ test/core.spec.js
@@ -19,6 +19,13 @@ describe('ssr', () => {
     expect(render('my-component', SimpleComponent, {message: 'hello'})).to.match(/<p>hello/)
   })
 
+  it('raw components can be rendered as div', function() {
+    const SimpleComponent = require('./tags/simple-component.riot').default
+    const result = render('div', SimpleComponent, {message: 'hello'})
+    expect(result).to.match(/<p>hello/)
+    expect(result).to.match(/is="simple-component"/)
+  })
+
   it('nested components can be rendered', function() {
     const ParentComponent = require('./tags/parent-component.riot').default
 
