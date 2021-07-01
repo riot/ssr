d5b06dfff2bfa0717557184a83c2541ce27ddd61
Giuliano Collacchioni <eliteXXL@hotmail.it>
2021-07-01 09:38:47 +0200

    make dom implementation disposable

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,22 @@
 import {parseHTML} from 'linkedom'
 
-const {
-  window,
-  document,
-  Node
-} = parseHTML('<!doctype html><html></html>')
+export function setup() {
+  const {
+    window,
+    document,
+    Node
+  } = parseHTML('<!doctype html><html></html>')
 
-global.window = window
-global.document = document
-global.Node = Node
+  global.window = window
+  global.document = document
+  global.Node = Node
+}
+
+export function dispose() {
+  if (global.window === undefined) {
+    return
+  }
+  global.window = undefined
+  global.document = undefined
+  global.Node = undefined
+}
